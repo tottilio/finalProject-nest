087e513f1e9a81dc90575bcd60dc599d4f9b6bfa
Totti-lnx <rodrigoguerrerogarcia777futbol@gmail.com>
2025-11-21 19:02:38 -0600

    feat: inicialice endpoints principals, co, s, mo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { AdminsModule } from './admins/admins.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { UsersModule } from './users/users.module';
+import { RoutesModule } from './routes/routes.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AdminsModule, TicketsModule, UsersModule, RoutesModule, LocationsModule], // DB
   controllers: [],
   providers: [],
 })
